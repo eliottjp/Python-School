86a402b11751e3143d8dade83e4ba87d6510c5e5
Eliott Pike <85697180+eliottjp@users.noreply.github.com>
2023-09-27 19:43:19 +0000

    27/09/23

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ MiddleName = input("Whats Your Middle Name? ")
 LastName = input("Whats Your Last Name? ")
 
 #Prints Name Together
-print("Your Name is,", FirstName , LastName,", Right?")
-#Prints Out Initials
-print('Your Intials Are, '+
-      FirstName.upper()[0],     #Grabbng the first letter of the first name, then converting it to uppercase
-      MiddleName.upper()[0],     #Grabbng the first letter of the middle name, converting it to uppercase
-      LastName[0].upper()+      #Getting The first letter of the last name and converting it to uppercase
-      LastName[1:].lower()      #Using the slicing function we get the rest of the letters starting from the seccond letter lowercase
-      )
+print("Your Name is,", FirstName, MiddleName, LastName+", Right?")
 
 
-if MiddleName == "":
-    
+if MiddleName == "":    #Checks if there is a input for middle name
+      print('Your Intials Are, '+
+            FirstName.upper()[0],     #Grabbng the first letter of the first name, then converting it to uppercase
+            LastName[0].upper()+      #Getting The first letter of the last name and converting it to uppercase
+            LastName[1:].lower()      #Using the slicing function we get the rest of the letters starting from the seccond letter lowercase
+            )
+else: 
+      print('Your Intials Are, '+
+            FirstName.upper()[0],     #Grabbng the first letter of the first name, then converting it to uppercase
+            MiddleName.upper()[0],     #Grabbng the first letter of the middle name, converting it to uppercase
+            LastName[0].upper()+      #Getting The first letter of the last name and converting it to uppercase
+            LastName[1:].lower()      #Using the slicing function we get the rest of the letters starting from the seccond letter lowercase
+            )
